a9fccca628516d2a831aa0cec7c441348c5fc79d
Johan <jaromerora@gmail.com>
2023-08-25 11:20:38 -0500

    second commit

--- SPWebUI/ClientApp/src/components/PredictDetail.js
+++ SPWebUI/ClientApp/src/components/PredictDetail.js
@@ -20,16 +20,15 @@ export class PredictDetail extends Component {
                                                     <th id="date"><abbr title="Creation Date">Date</abbr></th>
                                                     <th id="companyName">Company Name</th>
                                                     <th id="ticker"><abbr title="Ticker">Tkr</abbr></th>
-                                                    <th id="1d"><abbr title="One Month">1Month</abbr></th>
-                                                    <th id="1w"><abbr title="Six Months">6Month</abbr></th>
-                                                    <th id="1mo"><abbr title="One Year">1y</abbr></th>
-                                                    <th id="3mo"><abbr title="Five Years">5y</abbr></th>
+                                                    <th id="1m"><abbr title="One Month">1Month</abbr></th>
+                                                    <th id="6m"><abbr title="Six Months">6Month</abbr></th>
+                                                    <th id="1y"><abbr title="One Year">1y</abbr></th>
+                                                    <th id="5y"><abbr title="Five Years">5y</abbr></th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 <tr>
                                             <td>{ this.state.data.creationDate }</td>
-                                            <td>{this.state.data.ticker }</td>
                                             <td>{this.state.data.ticker }</td>
                                             <td>${this.state.data.OneMonth }</td>
                                             <td>${this.state.data.SixMonths }</td>
